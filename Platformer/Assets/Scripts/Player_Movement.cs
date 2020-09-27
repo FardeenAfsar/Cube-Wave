82f033f0794036745abe160652faa5b9e2465dab
Fardeen Afsar <afsarfardeen@gmail.com>
2020-09-27 18:11:20 -0400

    Added Collisions

--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 [RequireComponent (typeof(Controller2D))]
 public class Player_Movement : MonoBehaviour
 {
+    float moveSpeed = 6;
+    float gravity = -20;
+    Vector3 velocity;
+
     private Controller2D controller;
 
     void Start()
     {
         controller = GetComponent<Controller2D>();
+    }
+
+    void Update()
+    {
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
 }
