e874919db93f55fd7e22b8da294cc1e8b42819a8
Fardeen Afsar <afsarfardeen@gmail.com>
2020-09-27 15:41:56 -0400

    Rewriting movement

---
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed, jumpforce;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0) Flip();
+        else if (facingRight == true && moveInput < 0) Flip();
+        
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+}
