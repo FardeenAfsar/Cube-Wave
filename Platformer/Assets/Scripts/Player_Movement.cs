65b5add1f9c1e3264adb139afc2f9ae60405ca4a
Fardeen Afsar <afsarfardeen@gmail.com>
2020-09-27 16:53:29 -0400

    Raycast setup

--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -2,33 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Controller2D))]
 public class Player_Movement : MonoBehaviour
 {
-    public float speed, jumpforce;
-    private float moveInput;
-    private Rigidbody2D rb;
+    private Controller2D controller;
 
-    private bool facingRight = true;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<Controller2D>();
     }
 
-    void FixedUpdate()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-        if (facingRight == false && moveInput > 0) Flip();
-        else if (facingRight == true && moveInput < 0) Flip();
-        
-    }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 Scale = transform.localScale;
-        Scale.x *= -1;
-        transform.localScale = Scale;
-    }
 }
