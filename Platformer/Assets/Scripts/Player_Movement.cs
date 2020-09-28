569b6955c2fe7d9279e2e1d341e5b6217640e554
Fardeen Afsar <afsarfardeen@gmail.com>
2020-09-28 17:06:56 -0400

    Implemented Verlet velocity

--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -7,8 +7,8 @@ public class Player_Movement : MonoBehaviour
 {
     public float jumpHeight = 4;
     public float timeToJumpApex = 0.4f;
-    float accelerationTimeAir = .2f;
-    float accelerationTimeGrounded = 0.1f;
+    float accelerationTimeAir = .125f;
+    float accelerationTimeGrounded = 0.045f;
     float moveSpeed = 6;
     float gravity;
 
@@ -16,6 +16,13 @@ public class Player_Movement : MonoBehaviour
     Vector3 velocity;
     float velocityXSmoothing;
     private Controller2D controller;
+    //For Debug
+    float startHeight = Mathf.NegativeInfinity;
+    float maxHeightReached = Mathf.NegativeInfinity;
+    bool reachedApex = true;
+    float jumpTimer = 0;
+    //
+    Vector3 oldVelocity;
 
     void Start()
     {
@@ -23,6 +30,15 @@ public class Player_Movement : MonoBehaviour
 
         gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
         jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+    
+    void Jump()
+    {
+        jumpTimer = 0;
+        maxHeightReached = Mathf.NegativeInfinity;
+        velocity.y = jumpVelocity;
+        startHeight = transform.position.y;
+        reachedApex = false;
     }
 
     void Update()
@@ -37,12 +53,29 @@ public class Player_Movement : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.Z) && controller.collisions.below)
         {
-            velocity.y = jumpVelocity;
+            Jump();
         }
+        if (!controller.collisions.below && !reachedApex)
+        {
+            jumpTimer += Time.fixedDeltaTime;
+        }
+
         float targetVelocity = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocity, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAir);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-    }
+        oldVelocity = velocity;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocity, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAir);
+        velocity.y += gravity * Time.fixedDeltaTime;
+        Vector3 deltaPos = (oldVelocity + velocity) * 0.5f * Time.fixedDeltaTime; 
+        controller.Move(deltaPos);
+
+        if (!reachedApex && maxHeightReached > transform.position.y)
+        {
+            float delta = maxHeightReached - startHeight;
+            float error = jumpHeight - delta;
+            Debug.Log($"error: {error:F4}, delta: {delta:F4}, time: {jumpTimer:F4}, gravity: {gravity:F4}, jumpforce: {jumpVelocity:F4}");
+            reachedApex = true;
+        }
+
+        maxHeightReached = Mathf.Max(transform.position.y, maxHeightReached);
+    }   
 
 }
